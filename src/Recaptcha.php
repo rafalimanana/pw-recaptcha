b91ef75bad2a1281bb8a9ea0e341db04503ffc4c
rafalimanana <rafalimananafabrice@gmail.com>
2023-02-08 15:31:55 +0300

    maj

--- src/Recaptcha.php
+++ src/Recaptcha.php
@@ -13,7 +13,7 @@ class Recaptcha
     public function __construct($options = [])
     {
     	$secret = null;
-    	$recaptcha_token = null;
+    	$recaptcha_key = null;
     	$recaptcha_score_threshold = null;
 
     	if (!is_array($options)) {
@@ -23,30 +23,33 @@ class Recaptcha
     	if ($options && isset($options["secret"])) {
     		$secret = $options["secret"];
     	}
-    	if ($options && isset($options["recaptcha_token"])) {
-    		$recaptcha_token = $options["recaptcha_token"];
+    	if ($options && isset($options["recaptcha_key"])) {
+    		$recaptcha_key = $options["recaptcha_key"];
     	}
     	if ($options && isset($options["recaptcha_score_threshold"])) {
     		$recaptcha_score_threshold = $options["recaptcha_score_threshold"];
     	}
         $this->secret = $secret;
-        $this->recaptcha_token = $recaptcha_token;
+        $this->recaptcha_key = $recaptcha_key;
         $this->recaptcha_score_threshold = $recaptcha_score_threshold;
     }
     /**
+     * @param $recaptcha_token
      * @return boolean
      */
-    public function isValidReCaptcha(){
+    public function isValidReCaptcha(
+    	$recaptcha_token
+    ){
         if(
         	!$this->secret || 
-        	!$this->recaptcha_token || 
+        	!$recaptcha_token || 
         	!$this->recaptcha_score_threshold
         ){
             return false;
         }
 
         $recaptcha = $this->checkReCaptcha(
-        	$this->recaptcha_token,
+        	$recaptcha_token,
         	$this->secret
         );
 
